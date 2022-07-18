cbd07bb0729a875afd164f6efacac3b34593d6a5
unknown <mgespindola93@gmail.com>
2022-07-17 23:44:35 -0300

    Inicio projecto con Spring Boot

---
+++ Java Avanzado/Sesion-3-4-5/src/test/java/com/company/bootcampers/Sesion345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.bootcampers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sesion345ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
