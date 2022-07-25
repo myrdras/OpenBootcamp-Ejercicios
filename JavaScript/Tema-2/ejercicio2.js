48fb0795a9e4d9141c76b649793aee1ef7778f54
unknown <mgespindola93@gmail.com>
2022-07-24 23:54:07 -0300

    Ejercicio 2

---
+++ JavaScript/Tema-2/ejercicio2.js
@@ -0,0 +1,14 @@
+let nombre = "Myrdrass";
+let edad = 28;
+let esDesarrollador = true;
+let nacimiento = new Date(1993, 11, 4);
+let libro = {
+   titulo: "Clean Code: A Handbook of Agile Software Craftsmanship",
+   autor: "Robert C. Martin",
+   fecha: 2008,
+   url: "https://www.goodreads.com/book/show/3735293-clean-code",
+};
+
+const lista = [nombre, edad, esDesarrollador, nacimiento, libro];
+
+console.log(lista);
