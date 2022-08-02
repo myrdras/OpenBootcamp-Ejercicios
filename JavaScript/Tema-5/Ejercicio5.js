217cf6fa09c652fd85bbeb52b51b7ea85ace1bcd
unknown <mgespindola93@gmail.com>
2022-08-01 23:25:27 -0300

    Ejercicio 5

---
+++ JavaScript/Tema-5/Ejercicio5.js
@@ -0,0 +1,12 @@
+const altura = 163
+
+const alturaM = altura / 100 // 1.63
+
+const peso = 55.5
+
+const alturaArriba = alturaM.toFixed(1) // 1.6
+
+const pesoAbajo = Math.floor(peso) // 55
+
+
+const valor = (Number.MAX_VALUE + 1) === Number.MAX_VALUE // true
